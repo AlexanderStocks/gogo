9ca1f188cb94d7e70a5dc8474f82231735d2dbf5
astocks <astocks@microsoft.com>
2024-09-16 16:09:50 +0100

    Declaration statements and binary addition

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -56,8 +56,8 @@ func evalStmt(stmt ast.Stmt, fset *token.FileSet, env *runtime.Environment) erro
 		return err
 	// case *ast.AssignStmt:
 	// 	return evalAssignStmt(s, fset, env)
-	// case *ast.DeclStmt:
-	// 	return evalDeclStmt(s, fset, env)
+	case *ast.DeclStmt:
+		return evalDeclStmt(s, fset, env)
 	// case *ast.IfStmt:
 	// 	return evalIfStmt(s, fset, env)
 	// case *ast.ForStmt:
@@ -79,8 +79,8 @@ func evalExpr(expr ast.Expr, fset *token.FileSet, env *runtime.Environment) (int
 			}, nil
 		}
 		return env.Get(e.Name)
-	// case *ast.BinaryExpr:
-	//     return evalBinaryExpr(e, fset, env)
+	case *ast.BinaryExpr:
+		return evalBinaryExpr(e, fset, env)
 	case *ast.CallExpr:
 		return evalCallExpr(e, fset, env)
 	// case *ast.UnaryExpr:
@@ -138,5 +138,97 @@ func evalCallExpr(call *ast.CallExpr, fset *token.FileSet, env *runtime.Environm
 		return fn(args...)
 	default:
 		return nil, fmt.Errorf("attempt to call non-function: %T", fun)
+	}
+}
+
+func evalDeclStmt(declStmt *ast.DeclStmt, fset *token.FileSet, env *runtime.Environment) error {
+	genDecl, ok := declStmt.Decl.(*ast.GenDecl)
+	if !ok {
+		return fmt.Errorf("unsupported declaration: %T", declStmt.Decl)
+	}
+	for _, spec := range genDecl.Specs {
+		valueSpec, ok := spec.(*ast.ValueSpec)
+		if !ok {
+			return fmt.Errorf("unsupported spec: %T", spec)
+		}
+		for i, name := range valueSpec.Names {
+			var value interface{}
+			if i < len(valueSpec.Values) {
+				val, err := evalExpr(valueSpec.Values[i], fset, env)
+				if err != nil {
+					return err
+				}
+				value = val
+			} else {
+				value = getZeroValue(valueSpec.Type)
+			}
+			env.Set(name.Name, value)
+		}
+	}
+	return nil
+}
+
+func getZeroValue(expr ast.Expr) interface{} {
+	switch expr.(type) {
+	case *ast.Ident:
+		// Handle built-in types
+		return 0
+	default:
+		return nil
+	}
+}
+
+func evalSpec(spec ast.Spec, fset *token.FileSet, env *runtime.Environment) (interface{}, error) {
+	switch s := spec.(type) {
+	case *ast.ValueSpec:
+		if len(s.Values) == 0 {
+			return nil, nil
+		}
+		return evalExpr(s.Values[0], fset, env)
+	default:
+		return nil, fmt.Errorf("unsupported spec type: %T", s)
+	}
+}
+
+func evalBinaryExpr(expr *ast.BinaryExpr, fset *token.FileSet, env *runtime.Environment) (interface{}, error) {
+	left, err := evalExpr(expr.X, fset, env)
+	if err != nil {
+		return nil, err
+	}
+	right, err := evalExpr(expr.Y, fset, env)
+	if err != nil {
+		return nil, err
+	}
+	switch expr.Op {
+	case token.ADD:
+		return add(left, right)
+	// case token.SUB:
+	// 	return sub(left, right)
+	// case token.MUL:
+	// 	return mul(left, right)
+	// case token.QUO:
+	// 	return quo(left, right)
+	// case token.EQL:
+	// 	return eql(left, right)
+	// case token.LSS:
+	// 	return lss(left, right)
+	// case token.GTR:
+	// 	return gtr(left, right)
+	default:
+		return nil, fmt.Errorf("unsupported binary operator: %v", expr.Op)
+	}
+}
+
+func add(left, right interface{}) (interface{}, error) {
+	switch l := left.(type) {
+	case int64:
+		switch r := right.(type) {
+		case int64:
+			return l + r, nil
+		default:
+			return nil, fmt.Errorf("unsupported right operand type: %T", right)
+		}
+	default:
+		return nil, fmt.Errorf("unsupported left operand type: %T", left)
 	}
 }
